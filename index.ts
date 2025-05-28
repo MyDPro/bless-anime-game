9470de0b4b4f8bfdb0fda650bdbcc8d433627ac3
MyD <m.demir-21@outlook.com>
2025-05-29 02:27:00 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const server = new WebServer();
 
 try {
   server.statics('public', '/');
-  server.start(3000); // bls.toml'deki port ile uyumlu
+  server.start(); // bls.toml'deki port ile uyumlu
   console.log('Blockless web sunucusu başlatıldı: http://localhost:3000');
 } catch (error) {
   console.error('Web sunucusu başlatma hatası:', error);
